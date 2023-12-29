70782228d9438c52fda091fe14055566ffcbe9f6
Douglas Butner <dougbutner@gmail.com>
2023-12-28 20:08:51 -0600

    FIXED orbit lengths

--- include/time.hpp
+++ include/time.hpp
@@ -16,13 +16,16 @@ enum class In {
     Week = 604800,               // Week
     Month = 2592000,             // Month (approximated as 30 days)
     Year = 31536000              // Year (approximated as 365 days, ignoring leap years)
+
     /*/ --- Celestial Orbits --- /*/
-    MercuryOrbit = 57600000,     // One orbit of Mercury around the Sun (approximated as 57.6 days)
-    VenusOrbit = 2432000000,      // One orbit of Venus around the Sun (approximated as 243.2 days)
-    MarsOrbit = 668560000,        // One orbit of Mars around the Sun (approximated as 668.56 days)
-    JupiterOrbit = 13107200000,   // One orbit of Jupiter around the Sun (approximated as 1310.72 days)
-    SaturnOrbit = 29920000000,    // One orbit of Saturn around the Sun (approximated as 2992 days)
-    UranusOrbit = 84372000000,    // One orbit of Uranus around the Sun (approximated as 84372 days)
-    NeptuneOrbit = 165680000000,  // One orbit of Neptune around the Sun (approximated as 16568 days)
-    AsteroidBeltOrbit = 388800000 // Approx orbit of an asteroid in the main belt (about 4 years)
+    MercuryOrbit = 4976640,     // One orbit of Mercury around the Sun (approximated as 57.6 days)
+    VenusOrbit = 21012480,      // One orbit of Venus around the Sun (approximated as 243.2 days)
+    MarsOrbit = 59318780,        // One orbit of Mars around the Sun (approximated as 686.56 days)
+    JupiterOrbit = 374371200,   // One orbit of Jupiter around the Sun (approximated as 4,333 days)
+    SaturnOrbit = 929577600,    // One orbit of Saturn around the Sun (approximated as 30,687 days)
+    UranusOrbit = 2651357000,    // One orbit of Uranus around the Sun (approximated as 30687 days)
+    NeptuneOrbit = 5200416000,  // One orbit of Neptune around the Sun (approximated as 60190 days)
+    PlutoOrbit = 7824384000,     // One orbit of Pluto around the Sun (approximated as 90560 days)
+    AsteroidBeltOrbit = 12614400 // Approx orbit of an asteroid in the main belt (approximated as 1460 days )
+
 };
